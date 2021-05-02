928494314f08bf5e5d2694216979c5d14277dae8
Semih Atalay <semihatalay23@gmail.com>
2021-05-02 18:22:17 +0300

    Business katmanına KategoriManager class'ı eklendi. İçine ekleme fonsiyonu yazıldı.

---
+++ Project.Business/Kategori/KategoriManager.cs
@@ -0,0 +1,21 @@
+﻿using Project.DataAccess.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Business.Kategori
+{
+    public class KategoriManager
+    {
+        WebAutomationEntities db = new WebAutomationEntities();
+        public void KategoriEkle(string kategoriAdi)
+        {
+            Kategoriler kat = new Kategoriler();
+            kat.KategoriAd = kategoriAdi;
+            db.Kategoriler.Add(kat);
+            db.SaveChanges();
+        }
+    }
+}
